6f6e4ef8a25a466b0350632e24c76593c3e6aa47
HitBlast <hitblastlive@gmail.com>
2025-06-02 14:43:11 +0600

    fixed brew_install clippy error

--- src/commands/brew_install.rs
+++ src/commands/brew_install.rs
@@ -132,7 +132,7 @@ pub async fn run(verbose: bool, dry_run: bool) -> Result<()> {
     if dry_run {
         for args in &install_tasks {
             let display = format!("brew {}", args.join(" "));
-            print_log(LogLevel::Dry, &format!("{}", display));
+            print_log(LogLevel::Dry, &display);
         }
     } else {
         // execute installs concurrently
