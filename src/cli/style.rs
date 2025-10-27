4393df829f0c13ab450f3108ab6426827effa571
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-10-27 09:23:26 +0600

    remove unnecessary headers

--- src/cli/style.rs
+++ src/cli/style.rs
@@ -5,18 +5,8 @@ use anstyle::{AnsiColor, Color::Ansi, Effects, Style};
 /// Style attributes for cutler CLI.
 pub fn get_styles() -> clap::builder::Styles {
     clap::builder::Styles::styled()
-        .usage(
-            Style::new()
-                .effects(Effects::CURLY_UNDERLINE)
-                .bold()
-                .fg_color(Some(Ansi(AnsiColor::White))),
-        )
-        .header(
-            Style::new()
-                .bold()
-                .bg_color(Some(Ansi(AnsiColor::White)))
-                .fg_color(Some(Ansi(AnsiColor::Black))),
-        )
+        .usage(Style::new().effects(Effects::CURLY_UNDERLINE).bold())
+        .header(Style::new().effects(Effects::CURLY_UNDERLINE).bold())
         .literal(Style::new().bold())
         .invalid(Style::new().bold().fg_color(Some(Ansi(AnsiColor::Red))))
         .error(Style::new().bold().fg_color(Some(Ansi(AnsiColor::Red))))
